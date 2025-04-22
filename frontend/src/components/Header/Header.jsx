25024a9d749025a2c08409860cb23a824c29f743
Abhishek jha <abhishek.chhatauni@gmail.com>
2025-04-22 10:00:31 +0530

    headerImage_error solved

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,7 +3,9 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className='header'>
+        <div className='header'
+            style={{ backgroundImage: "url('/header_img.jpg')", backgroundRepeat: 'no-repeat' }}
+        >
             <div className="header-contents">
                 <h2>Order your <span><a href="#explore-menu">favourite</a></span> food here</h2>
                 <p>For over a decade, we’ve enabled our customers to discover new tastes, delivered right to their doorstep</p>
